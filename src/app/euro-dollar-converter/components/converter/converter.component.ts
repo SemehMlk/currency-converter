89ea8112dfd0bf488f3a0e119984c5ff48a1ed46
SemehMlk <semehmliki@gmail.com>
2023-10-11 21:09:25 +0200

    optimize code

--- src/app/euro-dollar-converter/components/converter/converter.component.ts
+++ src/app/euro-dollar-converter/components/converter/converter.component.ts
@@ -9,39 +9,38 @@ import { History } from '../../history';
 })
 export class ConverterComponent implements OnInit {
 
-  exchangeRate: number = 1.1;
-  fixedRate: number;
-  fixedRateActive: boolean = false;
-  initialExchangeRate: number = 1.1;
-  fromAmount: number = 1;
-  convertedAmount: number = this.fromAmount * this.exchangeRate;
-  isEuroToUsd: boolean = true;
+  exchangeRate = 1.1;
+  fixedRate: number | null = null;
+  fixedRateActive = false;
+  initialExchangeRate = 1.1;
+  fromAmount = 1;
+  convertedAmount = this.fromAmount * this.exchangeRate;
+  isEuroToUsd = true;
 
   displayedColumns: string[] = ['realRate', 'userRate', 'initialAmount', 'convertedAmount'];
   dataSource = new MatTableDataSource<History>();
   history: History[] = [];
 
   ngOnInit() {
-    setInterval(() => {
-      const randomChange = (Math.random() * 0.1 - 0.05) * this.initialExchangeRate;
-      this.exchangeRate = this.initialExchangeRate + randomChange;
-      if (this.fixedRateActive && this.fixedRate) {
-        const rateDifference = Math.abs(this.exchangeRate - this.fixedRate);
-        const rateDifferencePercentage = (rateDifference / this.exchangeRate) * 100;
-        if (rateDifferencePercentage > 2) {
-          this.fixedRateActive = false;
-        }
+    setInterval(this.updateExchangeRate.bind(this), 3000);
+  }
+
+  updateExchangeRate() {
+    const randomChange = (Math.random() * 0.1 - 0.05) * this.initialExchangeRate;
+    this.exchangeRate = this.initialExchangeRate + randomChange;
+
+    if (this.fixedRateActive && this.fixedRate) {
+      const rateDifference = Math.abs(this.exchangeRate - this.fixedRate);
+      const rateDifferencePercentage = (rateDifference / this.exchangeRate) * 100;
+      if (rateDifferencePercentage > 2) {
+        this.fixedRateActive = false;
       }
-      this.updateConversion();
-    }, 3000);
+    }
+    this.updateConversion();
   }
 
   updateConversion() {
-    if (this.isEuroToUsd) {
-      this.convertedAmount = this.fromAmount * this.exchangeRate;
-    } else {
-      this.convertedAmount = this.fromAmount / this.exchangeRate;
-    }
+    this.convertedAmount = this.isEuroToUsd ? this.fromAmount * this.exchangeRate : this.fromAmount / this.exchangeRate;
     this.addToHistory();
   }
 
@@ -52,13 +51,16 @@ export class ConverterComponent implements OnInit {
 
   addToHistory() {
 
+    const fromCurrency = this.isEuroToUsd ? '€' : '$';
+    const toCurrency = this.isEuroToUsd ? '$' : '€';
+
     const historyItem: History = {
       realRate: this.exchangeRate,
       userRate: this.exchangeRate,
       initialAmount: this.fromAmount,
       convertedAmount: this.convertedAmount,
-      fromCurrency: this.isEuroToUsd ? '€' : '$',
-      toCurrency: this.isEuroToUsd ? '$' : '€'
+      fromCurrency,
+      toCurrency,
     };
 
     this.history.unshift(historyItem);
@@ -67,13 +69,12 @@ export class ConverterComponent implements OnInit {
       this.history.pop();
     }
 
-    this.dataSource = new MatTableDataSource<any>(this.history);
+    this.dataSource.data = this.history;
 
   }
 
   toggleFixedRate() {
     this.fixedRateActive = !this.fixedRateActive;
-
     this.updateConversion();
   }
 }
